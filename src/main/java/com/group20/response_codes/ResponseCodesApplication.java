333c14b7360e83899419c0cb154384043d96b2ed
admin <alex.nester2477@gmail.com>
2021-10-17 20:37:04 +0300

    Start

---
+++ src/main/java/com/group20/response_codes/ResponseCodesApplication.java
@@ -0,0 +1,13 @@
+package com.group20.response_codes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponseCodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResponseCodesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group20/response_codes/ResponseCodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group20.response_codes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResponseCodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
